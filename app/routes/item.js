45f03937445ff5934ff8afc20f0c52b9da3564e7
akshy <akshy.nattu@gmail.com>
2021-05-22 21:20:17 +0530

    First commit

---
+++ app/routes/item.js
@@ -0,0 +1,9 @@
+import Route from '@ember/routing/route';
+
+export default class ItemRoute extends Route {
+    model(param) {
+        const {item_id} = param;
+        return item_id;
+        
+    }
+}
