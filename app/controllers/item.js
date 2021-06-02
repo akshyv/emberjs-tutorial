e2113717e4dc2f92e368261523ab89627a2131a0
akshy <akshy.nattu@gmail.com>
2021-06-02 22:08:26 +0530

    After Nike

--- app/controllers/item.js
+++ app/controllers/item.js
@@ -3,14 +3,14 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class ItemController extends Controller {
-    @tracked color = 'red';
+  @tracked color = this.model.colors[0].color;
 
-    get productImage() {
-        return `/assets/images/beats-solo-${this.color}.png`;
-    }
+  get productImage() {
+    return this.model.colors.find(({ color }) => color === this.color).image;
+  }
 
-    @action
-    onChangeColor(newColor) {
-        this.color = newColor;
-    }
-} 
+  @action
+  onChangeColor(newColor) {
+    this.color = newColor;
+  }
+}
