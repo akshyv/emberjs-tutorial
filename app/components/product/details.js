da9732abe94368599108c4d2270704a609eb3279
akshy <akshy.nattu@gmail.com>
2021-06-12 22:32:16 +0530

    after cart

---
+++ app/components/product/details.js
@@ -0,0 +1,17 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+
+export default class ProductDetailsComponent extends Component {
+    
+    @service('shopping-cart') cart;
+
+    @action
+    addToCart() {
+        const { name,price} = this.args;
+        this.cart.addItem({
+            name,
+            price,
+        });
+    }
+}
---
+++ app/helpers/currency.js
@@ -0,0 +1,16 @@
+import { helper } from '@ember/component/helper';
+
+export default helper(function currency(params, hash = {}) {
+// {{currency 25}}
+const [number] = params;
+const {
+ sign = '₹'
+} = hash;
+const dollars = Math.floor(number);
+let cents = Math.floor(number * 100 % 100);
+
+if (cents.toString().length === 1) {
+  cents = '0' + cents;
+}
+return `${sign}${dollars}.${cents}`;
+});
